c1ddada72e75bf223ab4a46f6629d4ed081fa25f
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-10-05 14:16:04 -0300

    feat: alteracao na tela de compra de tickets

--- app/Views/adm/prad.php
+++ app/Views/adm/prad.php
@@ -14,7 +14,7 @@
   <li class="nav-item nav-item-prad">
     <a class="nav-link active" href="<?= base_url('student') ?>" aria-current="page">Painel do Estudante</a>
   </li>
-  <li class="nav-item nav-item-menu">
+  <li class="nav-item nav-item-prad">
     <a class="nav-link active" href="<?= base_url('menu') ?>" aria-current="page">Cadastrar Menu Semanal</a>
   </li>
 </ul>
--- app/Views/student/buytickets.php
+++ app/Views/student/buytickets.php
@@ -3,39 +3,53 @@
 <?php endif; ?>
 
 
-<div class="buy-tickets">
-    <h1>Comprar Tickets</h1>
 
-    <div class="tickets-quantities">
-        <button id="decrement" class="buttons">-</button>
-        <h2 id="quantitie">0</h2>
-        <button id="increment" class="buttons">+</button>
-    </div>
+    <?= form_open(''); ?>
+        <div class="buy-tickets">
+            <h1>Comprar Tickets</h1>
 
-    <div class="valor"> 
-        <h2>R$</h2>
-        <h2 class="preco">0</h2>
-    </div>
-</div>
+            <div class="tickets-quantities">
+                <button id="decrement" class="greg-button buttons" circle="" primary="" type="button" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <rect class="svg" width="14" height="2" x="5" y="11" fill="#ffffff" fill-rule="evenodd" rx="1"></rect>
+                    </svg>
+                </button>
+                <h2 id="quantitie">1</h2>
+                <button id="increment" class="greg-button buttons" circle="" primary="" type="button" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path class="svg" fill="#ffffff" fill-rule="evenodd" d="M13 11h5a1 1 0 0 1 0 2h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5V6a1 1 0 0 1 2 0v5z"></path>
+                    </svg>
+                </button>
+            </div>
+            <button class="btn btn-primary" type="submit">Pagar</button>
+        </div>
+    <?= form_close(); ?>
 
-</div>
 
 <script> 
+        
         var buttonsSetValues = document.querySelectorAll(".buttons");
-        var quantitieTickets = document.querySelector("#quantitie");
 
         function setValueSale(button) {
+            var quantitieTicketsField = document.querySelector("#quantitie");
+            
             if(button.id == 'decrement' ){
-                if(!(quantitieTickets.innerText == 0)) {
-                    
+                if(!(quantitieTicketsField.innerText == 0)) {
+                    var quantitieTicketsSale            =  parseInt(quantitieTicketsField.innerText);
+                        quantitieTicketsField.innerText     = quantitieTicketsSale - 1;
                 }
+            } else if (button.id == 'increment'){
+                var quantitieTicketsSale                =  parseInt(quantitieTicketsField.innerText);
+                    quantitieTicketsField.innerText     = quantitieTicketsSale + 1;
             }
         }
 
         buttonsSetValues.forEach(button => {
-            setValueSale(button);
+            button.addEventListener('click', function() {
+                setValueSale(button);
+            })
         })
 
-        console.log(buttonsSetValues)
+        
 
 </script>
