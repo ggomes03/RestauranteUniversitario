3e16595acf50815954d59e25ed2d8aaecd611238
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-11-09 17:21:36 -0300

    feat: relatorio de quantidade usada

--- app/Views/adm/report.php
+++ app/Views/adm/report.php
@@ -1,102 +1,149 @@
-<!-- <div class="tables-extracts-report">
-    <h1>Dias Mais Usados</h1>
-    <table class="table table-sm report-table" border="1"  >
-            <thead>
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">Data</th>
-                    <th scope="col">Valor</th>
-                    <th scope="col">Quantidade Vendida</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-
-    php foreach ($reports as $report) {  ?>
-                <tr>
-                    <th scope="row">
-
-                    </th>
-                    <td>
-                        
-                        <= date('d/m/y l', strtotime($report->dtVenda)) ?>
-
-                    </td>
-                    <td>
-                        
-                            < $report->valor ?> 
-                    </td>
-                    <td>
-                        <= $report->qtdVendida ?> 
-
-                    </td>
-                </tr>
-    php } ?>
-            </tbody>
-    </table>
-</div> -->
-
-<div class="filter-container">
-    <label for="monthFilter">Filtrar por Mês:</label>
-    <input type="text" id="monthFilter" placeholder="MM/AAAA">
-    <button onclick="filterTable()">Filtrar</button>
+<div class="modal" id="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Erro!</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Insira uma data válida</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 
+<h1 class="report-h1">Dias Mais Usados</h1>
+
+<div class="filter-container">
+    <label class="form-label" for="monthFilter">Filtrar por Mês:</label>
+    <div>
+        <input type="text" class="form-control" maxlength="10" id="dataInput" placeholder="MM/AAAA" autocomplete="off">
+        <button class="btn btn-primary" id="filter" onclick="filterTable()">Filtrar</button>
+    </div>
+</div>
 <div class="tables-extracts-report">
-    <h1>Dias Mais Usados</h1>
+   
     <table class="table table-sm report-table" border="1">
         <thead>
             <tr>
-                <th scope="col"></th>
-                <th scope="col" onclick="sortTable(1)">Data</th>
-                <th scope="col" onclick="sortTable(2)">Valor</th>
-                <th scope="col" onclick="sortTable(3)">Quantidade Vendida</th>
+                <th scope="col" onclick="sortTable(0)">Data</th>
+                <th scope="col" onclick="sortTable(1)">Valor</th>
+                <th scope="col" onclick="sortTable(2)">Quantidade Vendida</th>
             </tr>
         </thead>
-        <tbody class="table-group-divider">
+        <tbody class="table-group-divider datas-table">
             <?php foreach ($reports as $report) {  ?>
-                <tr>
-                    <th scope="row"></th>
-                    <td><?= date('d/m/Y l', strtotime($report->dtVenda)) ?></td>
+                <tr class="tr-data-table">
+                    <td class="dateUsed"><?= date('d/m/Y', strtotime($report->dtVenda)) ?></td>
                     <td><?= $report->valor ?></td>
                     <td><?= $report->qtdVendida ?></td>
                 </tr>
             <?php } ?>
+
+            <tr class="">
+                    <td >Total</td>
+                    <td><?php 
+                        $sum = 0;
+                        foreach ($reports as $report) {
+                            $sum += $report->valor;
+                        }
+                        echo $sum;
+                        ?>
+                    </td>
+                    <td> 
+                        
+                    <?php 
+                        $sum = 0;
+                        foreach ($reports as $report) {
+                            $sum += $report->qtdVendida;
+                        }
+                        echo $sum;
+                        ?>
+                    </td>
+            </tr>
         </tbody>
     </table>
 </div>
 
 <script>
+    function filterTable(){
+        var dateInputValue = document.getElementById('dataInput').value;
+        var dateInputMonth = dateInputValue.substring(0,2);
+        var dateInputYear = dateInputValue.substring(3,7);
+
+        var currentDate = new Date()
+        var currentMonth = currentDate.getMonth() + 1;
+        var currentYear = currentDate.getFullYear();
+
+        if(dateInputMonth > currentMonth || dateInputYear > currentYear){
+            callModal()
+        } else {
+            var tbody = document.querySelector('.datas-table');
+            var trDataTable = document.querySelectorAll('.tr-data-table');
+
+            var elementsForInsertInContainer = []
+            
+            if(dateInputValue != ""){                 
+                datesToFilter = document.querySelectorAll(".dateUsed");
+                
+                datesToFilter.forEach(function(dt, index) {
+
+                    var day = dt.innerText.substring(0,2);
+                    var month = dt.innerText.substring(3,5);
+                    var year = dt.innerText.substring(6,10);
+                    var data = new Date(year, month, day);
+
+                    var monthSale = data.getMonth();
+                    var yearSale = data.getFullYear();
+                    
+                    if(monthSale == dateInputMonth && yearSale == dateInputYear){
+                        elementsForInsertInContainer.push(trDataTable[index])
+                    }
+                })
+
+                tbody.innerHTML = '';
+
+                elementsForInsertInContainer.forEach(function(el){
+                    tbody.appendChild(el)
+
+                })
+
+                document.getElementById('dataInput').value = '';
+            } else {
+                location.reload();
+            }
+        }
+    }
+
+    var currentColumn = -1;
     var ascending = true;
 
     function sortTable(columnIndex) {
         var table, rows, switching, i, x, y, shouldSwitch;
-        table = document.querySelector(".report-table");
+        table = document.querySelector('.report-table');
         switching = true;
 
         while (switching) {
             switching = false;
             rows = table.rows;
-            for (i = 1; i < (rows.length - 1); i++) {
-                shouldSwitch = false;
 
-                x = rows[i].getElementsByTagName("td")[columnIndex];
-                y = rows[i + 1].getElementsByTagName("td")[columnIndex];
+            for (i = 1; i < rows.length - 1; i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName('td')[columnIndex];
+                y = rows[i + 1].getElementsByTagName('td')[columnIndex];
 
                 var xValue = x.innerHTML.toLowerCase();
                 var yValue = y.innerHTML.toLowerCase();
 
-                if (ascending) {
-                    if (xValue > yValue) {
-                        shouldSwitch = true;
-                        break;
-                    }
-                } else {
-                    if (xValue < yValue) {
-                        shouldSwitch = true;
-                        break;
-                    }
+                if (ascending ? xValue > yValue : xValue < yValue) {
+                    shouldSwitch = true;
+                    break;
                 }
+                
             }
 
             if (shouldSwitch) {
@@ -105,31 +152,46 @@
             }
         }
 
-        ascending = !ascending;
+        // Inverte a direção para a próxima iteração apenas se mudarmos de coluna
+        if (currentColumn !== columnIndex) {
+            ascending = true;
+        } else {
+            ascending = !ascending;
+        }
+
+        // Atualiza a coluna atual
+        currentColumn = columnIndex;
     }
 
-    function filterTable() {
-        var input, filter, table, tbody, tr, td, i, txtValue;
-        input = document.getElementById("monthFilter");
-        filter = input.value.trim().toUpperCase();
-        table = document.querySelector(".report-table");
-        tbody = table.getElementsByTagName("tbody")[0];
-        tr = tbody.getElementsByTagName("tr");
+    function callModal() {
+        const myModal = new bootstrap.Modal(document.getElementById('modal'));
+        myModal.show();
+    }
 
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1]; // Segundo coluna contém a data
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                var dateParts = txtValue.split(' '); // Divide a data em partes
-                var monthYear = dateParts[0].toUpperCase(); // Obtém "MM/AAAA" da data
+    // Adiciona um listener para o evento de digitação no input
+    document.getElementById('dataInput').addEventListener('input', function (event) {
+        // Obtém o valor atual do input
+        var inputValue = event.target.value;
 
-                if (monthYear.indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
+        // Remove qualquer caractere não numérico
+        inputValue = inputValue.replace(/\D/g, '');
+
+        // Aplica a máscara de data
+        inputValue = mascaraData(inputValue);
+
+        // Atualiza o valor do input com a data mascarada
+        event.target.value = inputValue;
+    });
+
+    // Função de máscara de data
+    function mascaraData(data) {
+        if (data.length > 2) {
+            return data.substring(0, 2) + '/' + data.substring(2, 6)
+        } else {
+            return data;
         }
     }
+
+    
 </script>
 
--- app/Views/application/header.php
+++ app/Views/application/header.php
@@ -5,8 +5,10 @@
     <link rel="shortcut icon" href="<?= base_url('images/prato.png')?>" type="image/x-icon">
     <link rel="stylesheet" href="<?= base_url('css/header.css'); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
